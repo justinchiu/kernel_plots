17462efa06ecc5bcc5bccaf1213b863e696a62e3
Justin Chiu <justinchiu@g.harvard.edu>
2021-10-19 23:21:26 -0400

    .

--- large_lhmm_states_feats.py
+++ large_lhmm_states_feats.py
@@ -43,14 +43,21 @@ data = np.array([
     (144, 142, 154, 169, 178,), # 4:1
     (147, 146, 156, 180, 189,), # 8:1
 ])
+data = np.array([
+    #16k  8k   4k   2k 1k 
+    (144, 151, 155, 163, 175,), # softmax
+    (141, 142, 154, 159, 168,), # 2:1
+    (139, 145, 156, 169, 178,), # 4:1
+    (141, 143, 163, 180, 189,), # 8:1
+])
 
-df = pd.DataFrame(
+acc_df = pd.DataFrame(
     data.T,
     columns=["softmax", "2:1", "4:1", "8:1"],
     index=[16384, 8192, 4096, 2048, 1024],
 )
 
-g = sns.relplot(data=df, kind="line", linewidth=3, aspect=1.3)
+g = sns.relplot(data=acc_df, kind="line", linewidth=3, aspect=1.3)
 g.set_axis_labels("Number of states", "Valid PPL")
 g.legend.set_title("State:Rank")
 ax = g.axes[0][0]
@@ -67,13 +74,13 @@ data = np.array([
     (1284,  462, 259, 226, 276,), # 8:1 # rerun 1k
 ]) / 6519
 
-df = pd.DataFrame(
+speed_df = pd.DataFrame(
     data.T,
     columns=["softmax", "2:1", "4:1", "8:1"],
     index=[16384, 8192, 4096, 2048, 1024],
 )
 
-g = sns.relplot(data=df, kind="line", linewidth=3, aspect=1.3)
+g = sns.relplot(data=speed_df, kind="line", linewidth=3, aspect=1.3)
 g.set_axis_labels("Number of states", "Secs / Batch")
 g.legend.set_title("State:Rank")
 ax = g.axes[0][0]
@@ -89,6 +96,41 @@ ax = g.axes[0][0]
 ax.set_xscale("log", base=2)
 g.tight_layout()
 g.savefig("lhmm-states-features-speed.png")
+
+# joint plot
+pal = sns.color_palette("flare", 1) + sns.color_palette("crest", 3)
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16,6))
+sns.lineplot(
+    data=acc_df, linewidth=3, ax=ax1, legend=False, dashes=False,
+    palette=pal,
+)
+ax1.set_xlabel("Number of states")
+ax1.set_ylabel("Valid PPL")
+ax1.set_xscale("log", base=2)
+ax1.set(ylim=(135,195))
+sns.lineplot(
+    data=speed_df, linewidth=3, ax=ax2, dashes=False,
+    palette=pal,
+)
+ax2.set_xlabel("Number of states")
+ax2.set_ylabel("Secs / Batch")
+ax2.set_yscale("log", base=2)
+ax2.set_xscale("log", base=2)
+
+#ax1.legend().set_visible(False)
+ax2.legend().set_visible(False)
+
+handles, labels = ax2.get_legend_handles_labels()
+l4 = fig.legend(
+    bbox_to_anchor=(.5,1.02),
+    loc="upper center",
+    borderaxespad=0,
+    ncol=4,
+    title="State:Rank",
+)
+
+fig.savefig("lhmm-speed-acc-joint.png", bbox_inches="tight")
+plt.close(fig)
 
 
 data = np.array([
